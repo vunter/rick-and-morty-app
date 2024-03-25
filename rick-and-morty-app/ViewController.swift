fc51e115205a137fba61425cc918bcd9020bdd47
lysonjeada <lyscalefi@gmail.com>
2024-03-25 15:06:40 -0300

    Initial Commit

---
+++ rick-and-morty-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  rick-and-morty-app
+//
+//  Created by Amaryllis Rosemaria Baldrez Calefi on 25/03/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
